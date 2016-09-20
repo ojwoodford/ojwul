74400aa73f501f492bc58796b759b7d8d2235c89
Oliver J. Woodford <o.j.woodford.98@cantab.net>
2016-09-20 17:31:44 +0100

    Add more options to render_confusion

--- graphics/render_confusion.m
+++ graphics/render_confusion.m
@@ -15,7 +15,9 @@ function render_confusion(M, class_names, varargin)
 opts.cmap = '-gray'; 
 %opts.cmap = '-hot*'; % Alternative option
 opts.totals = false;
+opts.labels = true;
 opts.font_size = 0.4;  % Proportion of box height
+opts.font = 'Times';
 opts = vgg_argparse(opts, varargin);
 
 % Create a totals row at the bottom
@@ -25,27 +27,28 @@ end
 
 % Create the background image
 im = sc(kron(bsxfun(@times, M, 1./sum(M, 2)), ones(8)), opts.cmap, [0 1]);
-hIm = image(im);
+image(im);
 im = sc(im(1:8:end,1:8:end,:), 'rgb2gray');
 im = im(:,:,1);
 
 % Write the values
-[h w] = size(M);
-set(get(hIm, 'Parent'), 'FontName', 'Times', 'FontUnits', 'Normalized', 'FontSize', 1.2*opts.font_size/h);
+[h, w] = size(M);
 color = {'k', 'w'};
 hold on
 for x = 1:w
     for y = 1:h
-        text(x*8-3.5, y*8-3.5, sprintf('%g', M(y,x)), 'FontName', 'Times', 'Units', 'Data', 'FontUnits', 'Normalized', 'FontSize', opts.font_size/h, 'Color', color{1+(im(y,x)<0.5)}, 'HorizontalAlignment', 'center', 'VerticalAlignment', 'middle');
+        text(x*8-3.5, y*8-3.5, sprintf('%.2g', M(y,x)), 'FontName', opts.font, 'Units', 'Data', 'FontUnits', 'Normalized', 'FontSize', opts.font_size/h, 'Color', color{1+(im(y,x)<0.5)}, 'HorizontalAlignment', 'center', 'VerticalAlignment', 'middle');
     end
 end
 hold off
 
 % Write labels
-xlabel 'Output class'
-ylabel 'Ground truth class'
+if opts.labels
+    set(xlabel('Output class'), 'FontName', opts.font, 'FontUnits', 'Normalized', 'FontSize', 1.2*opts.font_size/h);
+    set(ylabel('Ground truth class'), 'FontName', opts.font, 'FontUnits', 'Normalized', 'FontSize', 1.2*opts.font_size/h);
+end
 if opts.totals
-    text(w*8+2, h*8-3.5, 'TOTAL', 'FontName', 'Times', 'Units', 'Data', 'FontUnits', 'Normalized', 'FontSize', opts.font_size/h, 'Color', 'k', 'HorizontalAlignment', 'left', 'VerticalAlignment', 'middle');
+    text(w*8+2, h*8-3.5, 'TOTAL', 'FontName', opts.font, 'Units', 'Data', 'FontUnits', 'Normalized', 'FontSize', opts.font_size/h, 'Color', 'k', 'HorizontalAlignment', 'left', 'VerticalAlignment', 'middle');
 end
 
 % Sort out the axes
@@ -59,9 +62,16 @@ end
 % Write the class names
 hold on
 for x = 1:numel(class_names)
-    % Right edge
-    text(w*8+2, x*8-3.5, class_names{x}, 'FontName', 'Times', 'Rotation', 45, 'Units', 'Data', 'FontUnits', 'Normalized', 'FontSize', opts.font_size/h, 'Color', 'k', 'HorizontalAlignment', 'left', 'VerticalAlignment', 'middle');
-    % Top edge
-    text(x*8-3.5, -2, class_names{x}, 'FontName', 'Times', 'Rotation', 45, 'Units', 'Data', 'FontUnits', 'Normalized', 'FontSize', opts.font_size/h, 'Color', 'k', 'HorizontalAlignment', 'left', 'VerticalAlignment', 'middle');
+    if opts.labels
+        % Right edge
+        text(w*8+2, x*8-3.5, class_names{x}, 'FontName', opts.font, 'Rotation', 45, 'Units', 'Data', 'FontUnits', 'Normalized', 'FontSize', opts.font_size/h, 'Color', 'k', 'HorizontalAlignment', 'left', 'VerticalAlignment', 'middle');
+        % Top edge
+        text(x*8-3.5, -2, class_names{x}, 'FontName', opts.font, 'Rotation', 45, 'Units', 'Data', 'FontUnits', 'Normalized', 'FontSize', opts.font_size/h, 'Color', 'k', 'HorizontalAlignment', 'left', 'VerticalAlignment', 'middle');
+    else
+        % Left edge
+        text(-2, x*8-3.5, class_names{x}, 'FontName', opts.font, 'Rotation', 45, 'Units', 'Data', 'FontUnits', 'Normalized', 'FontSize', opts.font_size/h, 'Color', 'k', 'HorizontalAlignment', 'right', 'VerticalAlignment', 'middle');
+        % Bottom edge
+        text(x*8-3.5, h*8+2, class_names{x}, 'FontName', opts.font, 'Rotation', 45, 'Units', 'Data', 'FontUnits', 'Normalized', 'FontSize', opts.font_size/h, 'Color', 'k', 'HorizontalAlignment', 'right', 'VerticalAlignment', 'middle');
+    end
 end
 hold off
