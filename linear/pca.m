b0ebe56161b7f9bea2d4daf334ac3449945da19e
Oliver J. Woodford <o.j.woodford.98@cantab.net>
2016-09-20 12:46:48 +0100

    Support data with nans

--- linear/pca.m
+++ linear/pca.m
@@ -11,14 +11,39 @@
 %    V - Mx1 list of eigen values associated with each dimension.
 
 function [T, X, V] = pca(X)
+% Check for NaNs
+if ~any(isnan(X(:)))
+    % Compute the mean and subtract
+    t = mean(X, 2);
+    X = bsxfun(@minus, X, t);
 
-% Compute the mean and subtract
-t = mean(X, 2);
-X = bsxfun(@minus, X, t);
+    % Do the economy svd, to avoid memory issues
+    [R, V] = svd(X, 'econ');
+    R = R';
 
-% Do the economy svd, to avoid memory issues
-[R, V] = svd(X, 'econ');
-R = R';
+    if nargout > 2
+        % Square the singular values to give eigenvalues
+        V = diag(V);
+        V = (V .* V) ./ size(X, 2);
+    end
+else
+    % Have NaNs!
+    % Compute the mean and subtract
+    t = mean(X, 2, 'omitnan');
+    X = bsxfun(@minus, X, t);
+    
+    % Compute the covariance matrix
+    sd = sqrt(mean(X .* X, 2, 'omitnan'));
+    C = corrcoef(X', 'rows', 'pairwise');
+    C = C .* (sd * sd');
+    
+    % Compute the eigen values and vectors
+    [R, V] = eig(C);
+    
+    % Reorder so largest first
+    V = flipud(diag(V));
+    R = R(:,end:-1:1)';
+end
 
 % Compose the transform
 T = [R -(R * t)];
@@ -26,11 +51,5 @@ T = [R -(R * t)];
 if nargout > 1
     % Apply the transform
     X = R * X;
-end
-
-if nargout > 2
-    % Square the singular values to give eigenvalues
-    V = diag(V);
-    V = V .* V;
 end
 end
