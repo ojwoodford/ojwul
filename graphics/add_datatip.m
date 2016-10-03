9620cb79cf4c8a9eb12f185dc606e0395d2af06e
Oliver J. Woodford <o.j.woodford.98@cantab.net>
2016-10-03 14:30:24 +0100

    Add add_datatip

---
+++ graphics/add_datatip.m
@@ -0,0 +1,58 @@
+%ADD_DATATIP Add datatip data to graphical objects
+%
+%   h = add_datatip(h, value)
+%   h = add_datatip(h, 'Name', value, ...)
+%
+% Example:
+%   add_datatip(plot(rand(3, 1)), 'Name', {'Tom', 'Dick', 'Harry'});
+%
+% Add data to graphical objects, for querying using datatips.
+%
+%IN:
+%   h - Scalar graphics object handle, to which to add the data.
+%   value - MxN numeric array or 1xN cell array of data, the ith column or
+%           cell of which is to be displayed when the ith datapoint is
+%           selected.
+%   'Name' - String indicating the name of the data value, so that
+%            name/value pairs can be used.
+%
+%OUT:
+%   h - The input scalar graphics handle, for inline usage (see example).
+
+function h = add_datatip(h, varargin)
+assert(isscalar(h), 'Only one handle expected');
+
+% Set the 'UserData' property of this object
+if nargin == 2
+    set(h, 'UserData', varargin{1});
+else
+    varargin = reshape(varargin, 2, []);
+    assert(all(cellfun(@ischar, varargin(1,:))), 'Name, value pairs expected');
+    set(h, 'UserData', varargin);
+end
+
+% Set the data tip function
+set(datacursormode(ancestor(h, 'Figure')), 'UpdateFcn', @datatip_txtfun);
+end
+
+function str = datatip_txtfun(~, h)
+[~, id] = sqdist2closest(h.Position', [h.Target.XData; h.Target.YData]);
+data = h.Target.UserData;
+str = struct('X', h.Target.XData(id), 'Y', h.Target.YData(id));
+if iscell(data)
+    for a = 1:2:numel(data)
+        if iscell(data{a+1})
+            str.(data{a}) = data{a+1}{id};
+        else
+            str.(data{a}) = data{a+1}(:,id)';
+        end
+    end
+else
+    if iscell(data)
+        str.data = data{id};
+    else
+        str.(data) = data(:,id)';
+    end
+end
+str = evalc('disp(str)');
+end
