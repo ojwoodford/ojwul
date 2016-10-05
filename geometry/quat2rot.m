d7881e57a75afad59402a9e3f481765a39deb0cb
Oliver J. Woodford <o.j.woodford.98@cantab.net>
2016-10-05 12:18:55 +0100

    Update help text

--- geometry/quat2rot.m
+++ geometry/quat2rot.m
@@ -1,10 +1,20 @@
-function R = quat2rot(Q)
 %QUAT2ROT Convert a quaternion to a 3x3 rotation matrix
 %
-% R = quat2rot(Q)
+%   R = quat2rot(Q)
+%
+% This function takes in quaternions of the form [w x y z]', such that Q =
+% [1 0 0 0]' corresponds to R = eye(3).
+%
+%IN:
+%   Q - 4xN array of quaternions in the form [w x y z]'.
+%
+%OUT:
+%   R - 3x3xN array of corresponding rotation matrices.
 
+function R = quat2rot(Q)
 R = quat_norm(Q);
 R = bsxfun(@times, reshape(R, 4, 1, []), reshape(R, 1, 4, []));
 R = [R(1,1,:)+R(2,2,:)-R(3,3,:)-R(4,4,:), 2*(R(2,3,:)-R(4,1,:)), 2*(R(2,4,:)+R(3,1,:));
      2*(R(2,3,:)+R(4,1,:)), R(1,1,:)-R(2,2,:)+R(3,3,:)-R(4,4,:), 2*(R(3,4,:)-R(2,1,:));
      2*(R(2,4,:)-R(3,1,:)), 2*(R(3,4,:)+R(2,1,:)), R(1,1,:)-R(2,2,:)-R(3,3,:)+R(4,4,:)];
+end
--- geometry/rot2quat.m
+++ geometry/rot2quat.m
@@ -1,17 +1,27 @@
 %ROT2QUAT Convert an array of 3x3 rotation matrices into quaternions
 %
 % Q = rot2quat(R)
+%
+%   R = quat2rot(Q)
+%
+% This function generates quaternions of the form [w x y z]', such that R =
+% eye(3) corresponds to Q = [1 0 0 0]'.
+%
+%IN:
+%   R - 3x3xN array of rotation matrices.
+%
+%OUT:
+%   Q - 4xN array of corresponding quaternions in the form [w x y z]'.
 
 function Q = rot2quat(R)
-
 M = cat(3, [1 0 0 0 0 0 0; 0 0 0 0 1 0 -1; 0 0 -1 0 0 1 0; 0 1 0 -1 0 0 0], ...
            [0 0 0 0 1 0 -1; 1 0 0 0 0 0 0; 0 1 0 1 0 0 0; 0 0 1 0 0 1 0], ...
            [0 0 -1 0 0 1 0; 0 1 0 1 0 0 0; 1 0 0 0 0 0 0; 0 0 0 0 1 0 1], ...
            [0 1 0 -1 0 0 0; 0 0 1 0 0 1 0; 0 0 0 0 1 0 1; 1 0 0 0 0 0 0]);
-       
 sz = size(R);
 R = reshape(R, 9, []);
 [m, n] = max([1, 1, 1; 1, -1, -1; -1, 1, -1; -1, -1, 1] * R([1 5 9],:));
 m = sqrt(1 + m) / 2;
 Q = sum(bsxfun(@times, M(:,:,n), shiftdim([m; bsxfun(@times, R([2 3 4 6 7 8],:), 1 ./ (4 * m))], -1)), 2);
 Q = reshape(Q, [4 sz(3:end) 1]);
+end
