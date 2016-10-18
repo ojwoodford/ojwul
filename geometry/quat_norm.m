6bc53caa4f08ac5a468a89f73151ae96d660ff50
Oliver J. Woodford <o.j.woodford.98@cantab.net>
2016-10-18 13:18:33 +0100

    Make singular quaternions the identity

--- geometry/quat_norm.m
+++ geometry/quat_norm.m
@@ -4,8 +4,9 @@ function Q = quat_norm(Q)
 Q = reshape(Q, 4, []);
 N = sum(Q .* Q);
 M = N == 0;
-Q(4,M) = 1;
+Q(1,M) = 1;
 M = ~M & N ~= 1;
 if any(M)
     Q(:,M) = bsxfun(@times, Q(:,M), 1./sqrt(N(M)));
+end
 end
