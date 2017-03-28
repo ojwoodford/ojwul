1e82735ccec4468edcc711cac98d270166fb4bb9
Oliver Woodford <o.j.woodford.98@cantab.net>
2017-03-28 15:53:37 -0700

    Support w last.

--- geometry/quat2rot.m
+++ geometry/quat2rot.m
@@ -1,17 +1,23 @@
 %QUAT2ROT Convert a quaternion to a 3x3 rotation matrix
 %
-%   R = quat2rot(Q)
+%   R = quat2rot(Q, [w_last])
 %
 % This function takes in quaternions of the form [w x y z]', such that Q =
-% [1 0 0 0]' corresponds to R = eye(3).
+% [1 0 0 0]' corresponds to R = eye(3). If w_last is true, quaternions are
+% of the form [x y z w].
 %
 %IN:
 %   Q - 4xN array of quaternions in the form [w x y z]'.
+%   w_last - boolean indicating whether quaternions should be of the form
+%            [x y z w]'. Default: false.
 %
 %OUT:
 %   R - 3x3xN array of corresponding rotation matrices.
 
-function R = quat2rot(Q)
+function R = quat2rot(Q, w_last)
+if nargin > 1 && w_last
+    Q = Q([4 1:3],:);
+end
 R = quat_norm(Q);
 R = bsxfun(@times, reshape(R, 4, 1, []), reshape(R, 1, 4, []));
 R = [R(1,1,:)+R(2,2,:)-R(3,3,:)-R(4,4,:), 2*(R(2,3,:)-R(4,1,:)), 2*(R(2,4,:)+R(3,1,:));
--- geometry/rot2quat.m
+++ geometry/rot2quat.m
@@ -1,19 +1,20 @@
 %ROT2QUAT Convert an array of 3x3 rotation matrices into quaternions
 %
-% Q = rot2quat(R)
-%
-%   R = quat2rot(Q)
+%    Q = rot2quat(R, [w_last])
 %
 % This function generates quaternions of the form [w x y z]', such that R =
-% eye(3) corresponds to Q = [1 0 0 0]'.
+% eye(3) corresponds to Q = [1 0 0 0]'. If w_last is true, quaternions are
+% of the form [x y z w].
 %
 %IN:
 %   R - 3x3xN array of rotation matrices.
+%   w_last - boolean indicating whether quaternions should be of the form
+%            [x y z w]'. Default: false.
 %
 %OUT:
-%   Q - 4xN array of corresponding quaternions in the form [w x y z]'.
+%   Q - 4xN array of corresponding quaternions.
 
-function Q = rot2quat(R)
+function Q = rot2quat(R, w_last)
 M = cat(3, [1 0 0 0 0 0 0; 0 0 0 0 1 0 -1; 0 0 -1 0 0 1 0; 0 1 0 -1 0 0 0], ...
            [0 0 0 0 1 0 -1; 1 0 0 0 0 0 0; 0 1 0 1 0 0 0; 0 0 1 0 0 1 0], ...
            [0 0 -1 0 0 1 0; 0 1 0 1 0 0 0; 1 0 0 0 0 0 0; 0 0 0 0 1 0 1], ...
@@ -24,4 +25,7 @@ R = reshape(R, 9, []);
 m = sqrt(1 + m) / 2;
 Q = sum(bsxfun(@times, M(:,:,n), shiftdim([m; bsxfun(@times, R([2 3 4 6 7 8],:), 1 ./ (4 * m))], -1)), 2);
 Q = reshape(Q, [4 sz(3:end) 1]);
+if nargin > 1 && w_last
+    Q = Q([2:4 1],:);
+end
 end
