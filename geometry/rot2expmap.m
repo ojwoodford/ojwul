de2e36280eabbbc8a893c2b9ba35d56b23610c61
Oliver Woodford <o.j.woodford.98@cantab.net>
2017-04-05 14:03:38 -0700

    Bug fix numerical error issue

--- geometry/rot2expmap.m
+++ geometry/rot2expmap.m
@@ -4,6 +4,7 @@
 
 function X = rot2expmap(R)
 sz = [size(R) 1];
-th = reshape(acos(1 - mod((0.5 * (1 - R(1,1,:) - R(2,2,:) - R(3,3,:)) - 1), 2)), [1 sz(3:end)]);
+th = reshape(acos(1 - mod((0.5 * (1 - min(R(1,1,:) + R(2,2,:) + R(3,3,:), 3)) - 1), 2)), [1 sz(3:end)]);
 X = reshape([R(3,2,:)-R(2,3,:); R(1,3,:)-R(3,1,:); R(2,1,:)-R(1,2,:)], [3 sz(3:end)]);
 X = bsxfun(@times, X, th ./ (sqrt(sum(X .* X, 1)) + 1e-300));
+end
