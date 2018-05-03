55806d78fc91d861fdf3f6e9951cb97fb9231b87
Oliver Woodford <o.j.woodford.98@cantab.net>
2018-05-03 11:37:21 -0700

    Change imnorm to zero-mean and unit variance local normalization

--- image/imnorm.m
+++ image/imnorm.m
@@ -1,24 +1,23 @@
 %IMNORM Spatially local image normalization
 %
-%   B = imnorm(A, sigma)
-%   B = imnorm(A, [szy szx])
-%   B = imnorm(..., nonlin)
+%   B = imnorm(A, sigma, noise_variance)
+%   B = imnorm(A, [szy szx], noise_variance)
 %
-% Apply a local normalization operator to an image, either with a Gaussian
-% or window average weighting.
+% Apply a local normalization operator (subtracting the mean and
+% normalizing the variance) to an image, either with a Gaussian or window
+% average weighting.
 %
 %IN:
 %   A - HxWxC input image.
 %   sigma - scalar indicating the standard deviation of the Gaussian
 %           weighting to apply, in pixels.
 %   [szy szx] - 1x2 window size for window average weighting.
-%   nonlin - a handle to an elementwise non-linear function to be applied
-%            to the denominator prior to normalization.
+%   noise_variance - scalar variance of image noise.
 %
 %OUT:
 %   B - HxWxC normalized image.
 
-function A = imnorm(A, sigma, nonlin)
+function A = imnorm(A, sigma, noise_variance)
 % Compute the separable filters
 if isscalar(sigma)
     fx = max(floor(5/2 * sigma), 1);
@@ -26,21 +25,19 @@ if isscalar(sigma)
     fx = fx / sum(fx);
     fy = fx;
 elseif isequal(size(sigma), [1 2]) && isequal(sigma, round(sigma))
-    fy = zeros(1, sigma(1)) + 1 / sigma(1);
-    fx = zeros(1, sigma(2)) + 1 / sigma(2);
+    fy = repmat(1 / sigma(1), sigma(1), 1);
+    fx = repmat(1 / sigma(2), 1, sigma(2));
 else
     error('sigma not recognized');
 end
 
 % Filter the image
 A = double(A);
-B = imfiltsep(A, fy, fx);
 
-% Apply the non-linear function
-if nargin > 2
-    B = nonlin(B);
-end
+% Subtract the mean
+A = A - imfiltsep(A, fy, fx);
 
-% Normalize the image
-A = A ./ B;
+% Divide by the standard deviation
+noise_variance = max(noise_variance, 1e-100);
+A = A ./ sqrt(imfiltsep(A .* A, fy, fx) + noise_variance);
 end
