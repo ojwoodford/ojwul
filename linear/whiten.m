2f21f09e94ec6c16ac3096e924a875b058d6d9f8
Oliver Woodford <o.j.woodford.98@cantab.net>
2018-05-03 11:37:31 -0700

    Update whiten

--- linear/whiten.m
+++ linear/whiten.m
@@ -1,4 +1,32 @@
-function x = whiten(x, dim)
-x = bsxfun(@minus, x, mean(x, dim));
-x = bsxfun(@times, x, 1./var(x, 0, dim));
+%WHITEN Transform data to be zero mean and identity covariance
+%
+%   [Y, T] = whiten(X, [epsilon])
+%
+%IN:
+%   X - MxN array of N vectors od dimension M to be whitened
+%   epsilon - scalar value to add to eigen values to avoid amplifying
+%             noise. Default: 1e-4.
+%
+%   Y - MxN array of whitened data.
+%   T - (M+1)x(M+1) matrix for the transformation [Y; 1] = T * [X; 1].
+
+function [X, T] = whiten(X, epsilon)
+% Set the default epsilon
+if nargin < 2
+    epsilon = 1e-4;
+end
+
+% Subtract the mean
+mu = mean(X, 1);
+X = bsxfun(@minus, X, mu);
+
+% Whitening the covariance
+[V, D] = svd(X * X');
+T = V' * diag(sqrt((size(X, 2) - 1) ./ (diag(D) + epsilon))) * V;
+X = T * X;
+
+% Construct the transform
+mu = T * -mu;
+T(end+1,end+1) = 1;
+T(1:end-1,end) = mu;
 end
