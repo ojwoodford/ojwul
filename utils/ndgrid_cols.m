a30b2a5d00d723fdc698fbef8de208fee30a4191
Oliver Woodford <oliver.woodford@snapchat.com>
2018-12-06 17:56:32 -0800

    Add sz output

--- utils/ndgrid_cols.m
+++ utils/ndgrid_cols.m
@@ -1,6 +1,6 @@
 %NDGRID_COLS Like NDGRID, but creates column vectors from the outputs
 %
-%   X = ndgrid_cols(...)
+%   [X, sz] = ndgrid_cols(...)
 %
 % This function applies passes its inputs directly to NDGRID, then converts
 % the outputs to row vectors, which are stacked vertically, so each
@@ -11,9 +11,12 @@
 %
 %OUT:
 %   X - (nargin)xN matrix of permutation column vectors.
+%   sz - 1xM array of sizes of outputs that ndgrid would produce with the
+%        same inputs, such that N = prod(sz).
 
-function X = ndgrid_cols(varargin)
+function [X, sz] = ndgrid_cols(varargin)
 X = cell(nargin, 1);
 [X{:}] = ndgrid(varargin{:});
+sz = size(X{1});
 X = cell2mat(cellfun(@(x) x(:)', X, 'UniformOutput', false));
 end
