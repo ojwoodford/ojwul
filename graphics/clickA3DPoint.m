3a72539d2fa5a1c6475218859e0f0bb481b8da50
Oliver Woodford <oliver.woodford@snapchat.com>
2019-04-17 21:35:00 -0700

    Add clickA3DPoint

---
+++ graphics/clickA3DPoint.m
@@ -0,0 +1,161 @@
+function h = clickA3DPoint(pointCloud, faces)
+%CLICKA3DPOINT
+%   H = CLICKA3DPOINT(POINTCLOUD) shows a 3D point cloud and lets the user
+%   select points by clicking on them. The selected point is highlighted 
+%   and its index in the point cloud will is printed on the screen. 
+%   POINTCLOUD should be a 3*N matrix, represending N 3D points. 
+%   Handle to the figure is returned.
+%
+%   other functions required:
+%       CALLBACKCLICK3DPOINT  mouse click callback function
+%       ROWNORM returns norms of each row of a matrix
+%       
+%   To test this function ... 
+%       pointCloud = rand(3,100)*100;
+%       h = clickA3DPoint(pointCloud);
+% 
+%       now rotate or move the point cloud and try it again.
+%       (on the figure View menu, turn the Camera Toolbar on, ...)
+%
+%   To turn off the callback ...
+%       set(h, 'WindowButtonDownFcn',''); 
+%
+%   by Babak Taati
+%   http://rcvlab.ece.queensu.ca/~taatib
+%   Robotics and Computer Vision Laboratory (RCVLab)
+%   Queen's University
+%   May 4, 2005 
+%   revised Oct 30, 2007
+%   revised May 19, 2009
+
+% Copyright (c) 2009, Babak Taati
+% All rights reserved.
+% 
+% Redistribution and use in source and binary forms, with or without
+% modification, are permitted provided that the following conditions are
+% met:
+% 
+%     * Redistributions of source code must retain the above copyright
+%       notice, this list of conditions and the following disclaimer.
+%     * Redistributions in binary form must reproduce the above copyright
+%       notice, this list of conditions and the following disclaimer in
+%       the documentation and/or other materials provided with the distribution
+% 
+% THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+% AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+% IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+% ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
+% LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+% CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+% SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+% INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+% CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+% ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+% POSSIBILITY OF SUCH DAMAGE.
+
+if nargin < 1
+    error('Requires one input arguments.')
+end
+
+if size(pointCloud, 1)~=3
+    error('Input point cloud must be a 3*N matrix.');
+end
+
+% show the point cloud
+h = gcf;
+plot3(pointCloud(1,:), pointCloud(2,:), pointCloud(3,:), 'c.');
+cameratoolbar('Show'); % show the camera toolbar
+%fcw();
+hold on; % so we can highlight clicked points without clearing the figure
+if nargin > 1
+    patch('Vertices', pointCloud', 'Faces', faces', 'FaceVertexCData', repmat([0.7 0.7 0.7], size(pointCloud, 2), 1), 'EdgeColor', 'k', 'LineStyle', '-', 'FaceColor', 'flat');
+end
+
+% set the callback, pass pointCloud to the callback function
+set(h, 'WindowButtonDownFcn', {@callbackClickA3DPoint, pointCloud});
+end
+
+function callbackClickA3DPoint(src, eventData, pointCloud)
+% CALLBACKCLICK3DPOINT mouse click callback function for CLICKA3DPOINT
+%
+%   The transformation between the viewing frame and the point cloud frame
+%   is calculated using the camera viewing direction and the 'up' vector.
+%   Then, the point cloud is transformed into the viewing frame. Finally,
+%   the z coordinate in this frame is ignored and the x and y coordinates
+%   of all the points are compared with the mouse click location and the 
+%   closest point is selected.
+%
+%   Babak Taati - May 4, 2005
+%   revised Oct 31, 2007
+%   revised Jun 3, 2008
+%   revised May 19, 2009
+
+point = get(gca, 'CurrentPoint'); % mouse click position
+camPos = get(gca, 'CameraPosition'); % camera position
+camTgt = get(gca, 'CameraTarget'); % where the camera is pointing to
+
+camDir = camPos - camTgt; % camera direction
+camUpVect = get(gca, 'CameraUpVector'); % camera 'up' vector
+
+% build an orthonormal frame based on the viewing direction and the 
+% up vector (the "view frame")
+zAxis = col(camDir/norm(camDir));    
+upAxis = col(camUpVect/norm(camUpVect)); 
+xAxis = cross(upAxis, zAxis);
+yAxis = cross(zAxis, xAxis);
+
+rot = [xAxis'; yAxis'; zAxis']; % view rotation 
+
+% the point cloud represented in the view frame
+rotatedPointCloud = rot * pointCloud; 
+
+% the clicked point represented in the view frame
+rotatedPointFront = rot * point' ;
+
+% find the nearest neighbour to the clicked point 
+pointCloudIndex = dsearchn(rotatedPointCloud(1:2,:)', ... 
+    rotatedPointFront(1:2));
+
+h = findobj(gca,'Tag','pt'); % try to find the old point
+selectedPoint = pointCloud(:, pointCloudIndex); 
+
+if isempty(h) % if it's the first click (i.e. no previous point to delete)
+    
+    % highlight the selected point
+    h = plot3(selectedPoint(1,:), selectedPoint(2,:), ...
+        selectedPoint(3,:), 'r.', 'MarkerSize', 20); 
+    set(h,'Tag','pt'); % set its Tag property for later use   
+
+else % if it is not the first click
+
+    delete(h); % delete the previously selected point
+    
+    % highlight the newly selected point
+    h = plot3(selectedPoint(1,:), selectedPoint(2,:), ...
+        selectedPoint(3,:), 'r.', 'MarkerSize', 20);  
+    set(h,'Tag','pt');  % set its Tag property for later use
+
+end
+
+fprintf('you clicked on point number %d\n', pointCloudIndex);
+end
+
+function v = rowNorm(A)
+%ROWNORM  norm of each row
+%   V = ROWNORM(A) returns the Euclidean norm of each row of A. When A is 
+%   an M*N matrix, the output, V, will be a M*1 vector.
+%
+%   Babak taati May 4, 2005
+%   revised May 19, 2009
+
+if nargin ~= 1
+    error('Requires one input arguments.')
+end
+
+v = sqrt(sum(A.*A, 2));
+end
+
+function A = col(A)
+A = A(:);
+end
+
