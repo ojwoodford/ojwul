e34c1081f2538743c03eb4f688e65a867c6d2151
Oliver Woodford <o.j.woodford.98@cantab.net>
2019-10-25 11:22:29 -0700

    Improve accuracy

--- linear/det33n.m
+++ linear/det33n.m
@@ -12,6 +12,12 @@
 
 function T = det33n(T)
 T = reshape(T, 9, []);
+cond = zeros(size(T));
+d = 1 ./ (1 + abs(T([1 5 9],:)));
+cond([1 5 9],:) = d;
+T = tmult(reshape(T, 3, 3, []), reshape(cond, 3, 3, []));
+T = reshape(T, 9, []);
 T = T([1 2 3 1 3 2],:) .* T([5 6 4 6 5 4],:) .* T([9 7 8 8 7 9],:);
-T = sum(T(1:3,:)) - sum(T(4:6,:));
+T = sum(T(1:3,:) - T(4:6,:));
+T = T ./ prod(d);
 end
