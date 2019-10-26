875939c643163b182b80e0916fd1396c8faf8d1f
Oliver Woodford <o.j.woodford.98@cantab.net>
2019-10-25 17:09:35 -0700

    Correctly return Inf for zero matrices

--- linear/inv33n.m
+++ linear/inv33n.m
@@ -16,5 +16,5 @@ sz = size(T);
 det = det33n(T);
 T = reshape(T, 9, []);
 T = T([5 8 2 7 1 4 4 7 1],:) .* T([9 3 6 6 9 3 8 2 5],:) - T([8 2 5 4 7 1 7 1 4],:) .* T([6 9 3 9 3 6 5 8 2],:);
-T = reshape(bsxfun(@times, T, 1 ./ det), sz);
+T = reshape(bsxfun(@times, (T + 1e-322), 1 ./ det), sz);
 end
