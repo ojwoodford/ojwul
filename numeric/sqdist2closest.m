717c2fc4275e8b0e647e523f9c27c56fc51ae608
Oliver Woodford <o.j.woodford.98@cantab.net>
2020-02-14 15:05:17 -0800

    Add computation in the other direction

--- numeric/sqdist2closest.m
+++ numeric/sqdist2closest.m
@@ -1,35 +1,73 @@
-function [D, I] = sqdist2closest(A, B)
+function [Da, Ia, Db, Ib] = sqdist2closest(A, B)
 %SQDIST2CLOSEST Squared Euclidean distance of closest point from B to A
 %
-%    [D I] = sqdist2closest(A, B)
+%    [Da, Ia, Db, Ib] = sqdist2closest(A, B)
 %
 % IN:
 %    A - MxJ matrix of columnwise vectors.
 %    B - MxK matrix of columnwise vectors.
 %
 % OUT:
-%    D - 1xJ Squared distance from each vector in A to their respective
+%    Da - 1xJ Squared distance from each vector in A to their respective
 %        closest vector in B.
-%    I - Indices of closest vectors from B.
+%    Ia - Indices of closest vectors from B.
+%    Db - 1xK Squared distance from each vector in B to their respective
+%        closest vector in A.
+%    Ib - Indices of closest vectors from A.
 
 % Precompute some things to speed up
-D = sum(A .* A, 1);
-B2 = sum(B .* B, 1);
-if numel(A) < numel(B)
-    A = A * 2;
+if numel(A) > numel(B)
+    flip = true;
+    [A, B] = deal(B, A);
 else
-    B = B * 2;
+    flip = false;
 end
+Da = sum(A .* A, 1);
+A = A * 2;
+B2 = sum(B .* B, 1);
+
+Ia = zeros(size(Da));
+Db = zeros(1, size(B, 2));
+Ib = zeros(size(Db));
 
 % Find distance to closest model points
-if nargout > 1
-    I = zeros(size(D));
+if flip || nargout > 2
+    if nargout > 2
+        Db = (B2 - A(:,1)' * B) + Da(1);
+        [Da(1), Ia(1)] = min(Db);
+        Ib(:) = 1;
+        for a = 2:size(A, 2)
+            D_ = (B2 - A(:,a)' * B) + Da(a);
+            [Da(a), Ia(a)] = min(D_);
+            Ib(D_ < Db) = a;
+            Db = min(Db, D_);
+        end
+    elseif nargout > 1
+        Db = (B2 - A(:,1)' * B) + Da(1);
+        Ib(:) = 1;
+        for a = 2:size(A, 2)
+            D_ = (B2 - A(:,a)' * B) + Da(a);
+            Ib(D_ < Db) = a;
+            Db = min(Db, D_);
+        end
+    else
+        Db = (B2 - A(:,1)' * B) + Da(1); 
+        for a = 2:size(A, 2)
+            Db = min(Db, (B2 - A(:,a)' * B) + Da(a));
+        end
+    end
+elseif nargout > 1
     for a = 1:size(A, 2)
-        [d, I(a)] = min(B2 - A(:,a)' * B);
-        D(a) = D(a) + d;
+        [d, Ia(a)] = min(B2 - A(:,a)' * B);
+        Da(a) = Da(a) + d;
     end
 else
     for a = 1:size(A, 2)
-        D(a) = D(a) + min(B2 - A(:,a)' * B);
+        Da(a) = Da(a) + min(B2 - A(:,a)' * B);
     end
+end
+
+if flip
+    [Da, Ia, Db, Ib] = deal(Db, Ib, Da, Ia);
+end
 end
