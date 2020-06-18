343bc29afa033c670a4205f421e305670c336eb3
Oliver Woodford <o.j.woodford.98@cantab.net>
2020-06-17 20:28:53 -0700

    Add option to limit the number of threads.

--- image/ojw_interp2.m
+++ image/ojw_interp2.m
@@ -3,6 +3,7 @@
 %	V = ojw_interp2(A, X, Y)
 %	V = ojw_interp2(A, X, Y, interp_mode)
 %	V = ojw_interp2(A, X, Y, interp_mode, oobv)
+%	V = ojw_interp2(A, X, Y, interp_mode, oobv, max_num_threads)
 %
 % 2d interpolation on a regular grid - similar to MATLAB's interp2() but
 % with much less overhead, and supports multiple channels and types. Note
@@ -12,6 +13,9 @@
 % spline employed by interp2(), which does, however, yield a smoother
 % interpolation.
 %
+% If compiled with OpenMP, this function splits the work across N threads,
+% where N is the smaller of max_num_threads and omp_get_num_procs().
+%
 %IN:
 %	A - HxWxC double, single, uint16, int16, uint8, int8 or Logical array.
 %	X - MxN horizontal offsets (1 being the centre of the first pixel).
@@ -19,7 +23,8 @@
 %	interp_mode - string, either 'nearest', 'linear', 'cubic', 'magic',
 %	              '4lanczos', or '6lanczos'. Default: 'linear'.
 %	oobv - 1x1 Out of bounds value. Default: NaN.
-%
+%   max_num_threads -  1x1 upper bound on the maximum number of threads to
+%                     be used. Default: 1024^2.
 %OUT:
 %	V - MxNxC interpolated values. Class is the same as that of oobv.
 %   G - 2xMxNxC array of gradients of V in x and y directions:
