0125b6b3a9492c3badc2360cc0ce27accec1eb03
Oliver Woodford <o.j.woodford.98@cantab.net>
2020-06-25 16:40:05 -0700

    Avoid subsref in col. Add help text.

--- utils/col.m
+++ utils/col.m
@@ -1,5 +1,17 @@
+%COL Convert an array to a column vector along a particular dimension
+%
+%   B = col(A, [dim])
+%
+%IN:
+%   A - Array of any size.
+%   dim - Positive integer indicating the dimension to arrange the elements
+%         of A along. Default: 1.
+%
+%OUT:
+%   B - Result of shiftdim(A(:), 1-dim).
+
 function x = col(x, dim)
-x = x(:);
+x = reshape(x, numel(x), 1);
 if nargin > 1
     x = shiftdim(x, 1-dim);
 end
